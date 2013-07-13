ef146a2bed67feed75930e59ab0c75ad106786dc
Antonel Ernest Pazargic <antonel.pazargic@gmail.com>
2013-07-13 23:27:13 +0300

    Validation for register an administrator.

--- html/script/administrator.js
+++ html/script/administrator.js
@@ -1,48 +1,66 @@
 $(function (){
-    $("#btn_inregistrare").click(function () {
-        var nume = $('#nume').val();
-        var prenume = $('#prenume').val();
-        var email = $('#email').val();
-        var telefon = $('#telefon').val();
-        var adresa= $('#adresa').val();
-        var username = $('#username').val();
-        var password = $('#password').val();
-        $.ajax(
-            {
-                type: "POST",
-                url: "inregistrare_admin.php",
-                dataType: "html",
-                data: "nume=" + nume + "&prenume=" + prenume + "&email=" + email + "&telefon=" + telefon + "&adresa=" + adresa +"&username=" + username + "&password=" + password,
-                success: function (result) {
-                    console.log(result);
-                    alert(result);
-                },
-                error: function () {
-                    alert("Inregistrarea a esuat!");
-                }
-            });
-    });
 
+    $("#register-form").validate({
+               rules: {
+                   nume: {
+                       required: true,
+                       minlength: 3
+                   },
+                   prenume: "required",
+                   telefon: {
+                       required: true,
+                       number: true,
+                       minlength: 10,
+                       maxlength: 15
+                   },
+                   adresa: "required",
+                   email: {
+                       required: true,
+                       email: true
+                   },
+                   username: "required"
+               },
+               messages: {
+                   nume: "Enter your first name, min 3 caractere",
+                   prenume: "Enter your last name",
+                   telefon: "Enter your phone",
+                   adresa: "Introduceti adresa",
+                   username: "Introduceti numele de utilizator",
+                   email: {
+                       required: "Introduceti adresa de email",
+                       email: "Va rugam sa introduceti o adresa de email valida."
+                   }
+               }
+           });
+           $("#register-form").submit(function (e) {
+               var valid = $("#register-form").valid();
+               console.log("Is form valid? " + valid);
+               if (valid) {
+                   inregistreazaAdministrator();
+               }
+               return false;
+           });
 
-  /*  $("#btn_logare").click(function () {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        console.log(username);
-        $.ajax(
-            {
-                type: "POST",
-                url: "check_login.php",
-                dataType: "html",
-                data: "username=" + username + "&password=" + password,
-                success: function (result) {
-                    console.log(result);
-                    alert(result);
-                },
-                error: function () {
-                    alert("Logare nereusita. Va rugam incercati mai tarziu!");
-                }
-            });
-
-    });*/
-
+           function inregistreazaAdministrator() {
+               var nume = $('#nume').val();
+               var prenume = $('#prenume').val();
+               var email = $('#email').val();
+               var telefon = $('#telefon').val();
+               var adresa = $('#adresa').val();
+               var username = $('#username').val();
+               var password = $('#password').val();
+               $.ajax(
+                   {
+                       type: "POST",
+                       url: "inregistrare_admin.php",
+                       dataType: "html",
+                       data: "nume=" + nume + "&prenume=" + prenume + "&email=" + email + "&telefon=" + telefon + "&adresa=" + adresa + "&username=" + username + "&password=" + password,
+                       success: function (result) {
+                           alert(result);
+                       },
+                       error: function () {
+                           alert("Inregistrarea a esuat!");
+                       }
+                   });
+           }
 });
